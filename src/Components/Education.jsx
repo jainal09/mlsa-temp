9ac51db6323bd21d960543ee99bf1afe9aa82b77
Jainal Gosaliya <jainal09gosalia@gmail.com>
2024-06-15 23:35:13 +0000

    chore: Add indent-rainbow extension and enable format on save
    Added Education Component

---
+++ src/Components/Education.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div id="education">
+      <div className="education-box">
+        <h2>Education</h2>
+        <h3>Northeastern University</h3>
+        <p>Masters of Software Engineering Systems </p>
+      </div>
+    </div>
+  );
+};
+export default Education;
